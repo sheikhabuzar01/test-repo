ec118fc6b2faf3a0c8ec31e069868324fac2ce18
sheikhabuzar01 <sheikhabuzar1122@gmail.com>
2026-03-30 02:31:34 +0500

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -56,7 +56,7 @@ resource "azurerm_kubernetes_cluster" "aks" {
     node_count     = 1
     
     # SWITCHING TO D-SERIES (More likely to have quota)
-    vm_size        = "Standard_D2as_v5" 
+    vm_size        = "Standard_A2_v2" 
     
     vnet_subnet_id = azurerm_subnet.aks_subnet.id
   }
